3648d617536e585aa928329e7f5bb1ef35c3327b
Fathi-Dz <fathi0504@student.abudzar.sch.id>
2026-01-27 10:44:01 +0700

    done

--- jeh project/src/App.jsx
+++ jeh project/src/App.jsx
@@ -1,10 +1,12 @@
 import Layout from "./Layout/Layout";
+import AlertButton from "./button/AlertButton";
 
 function App() {
   return (
     <Layout>
       {/* <h1>Welcome</h1>
       <p>Ini konten utama aplikasi gue.</p> */}
+      <AlertButton text="Alert Button" message="Ini pesan alert" />
     </Layout>
   );
 }
