b37db510c15a2dd1e51221961f559e9e1b016fc2
Fathi-Dz <fathi0504@student.abudzar.sch.id>
2026-02-06 08:50:23 +0700

    done

--- jeh project/src/App.jsx
+++ jeh project/src/App.jsx
@@ -2,6 +2,7 @@ import Layout from "./Layout/Layout";
 import AlertButton from "./button/AlertButton";
 import Table from "./Table/Table";
 import NoteApp from "./note/NoteApp";
+import StudentApp from "./stundent/StudentApp";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <AlertButton text="Alert Button" message="Ini pesan alert" />
         <Table />
         <NoteApp />
+        <StudentApp />
     </Layout>
   );
 }
